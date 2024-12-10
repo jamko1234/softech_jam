592ba3a20237902783b4198d6942c79b0051df8e
jam <kitonovmilan@gmail.com>
2024-12-10 23:46:07 +0300

    fixed some problems

--- softech/settings.py
+++ softech/settings.py
@@ -13,23 +13,26 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from datetime import timedelta
 import os
 from pathlib import Path
-from decouple import config
+import environ
+env = environ.Env()
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -94,14 +97,15 @@ WSGI_APPLICATION = 'softech.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='5432'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get("DB_NAME", "baiel_mistore"),
+        'USER': os.environ.get("DB_USER", "postgres"),
+        'PASSWORD': os.environ.get("DB_PASSWORD", ""),
+        'HOST': os.environ.get("DB_HOST", "localhost"),
+        'PORT': os.environ.get("DB_PORT", "5432")
     }
 }
+
 
 
 # Password validation
@@ -172,13 +176,14 @@ SIMPLE_JWT = {
     "REFRESH_TOKEN_LIFETIME": timedelta(minutes=120),
 }
 
-
-TELEGRAM_BOT_TOKEN = config('TELEGRAM_BOT_TOKEN')
-
 AUTH_USER_MODEL = "users.CustomUser"
 
-EMAIL_HOST = config("EMAIL_HOST")
-EMAIL_PORT = config("EMAIL_PORT")
-EMAIL_ADDRESS = config("EMAIL_ADDRESS")
-EMAIL_PASSWORD = config("EMAIL_PASSWORD")
+
+BOT_TOKEN = env("BOT_TOKEN", default="default_token")
+
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_ADDRESS = env("EMAIL_ADDRESS")
+EMAIL_PASSWORD = env("EMAIL_PASSWORD")
+
 
